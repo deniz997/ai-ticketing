ab9c496ffc0987e6cc111e3c386d7727e23a4816
i584237 <deniz.mert.tecimer@sap.com>
2024-08-16 20:23:50 +0200

    unnecessary files removed

--- kubernetes/count_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-original=$(find ./output_files -type f -name '*.yaml' | wc -l)
-total=$(find ./kustomize -type f -name '*.yaml' | wc -l)
-kustomization=$(find ./kustomize -type f -name 'kustomization.yaml' | wc -l)
-
-# from original we deducted one file because it contains a header only
-echo "Original Expected: $((original - 1)) vs Current: [kustomization: $kustomization, total: $total] => $(($total - $kustomization))"
